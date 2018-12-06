7579b6f509ace35c865cf768ccf4186f945f4319
shenzhongwu <1263955683@qq.com>
2018-12-06 15:39:23 +0800

    Initial commit

---
+++ cloud-eureka/src/main/java/com/poop/application/CloudEurekaApplication.java
@@ -0,0 +1,12 @@
+package com.poop.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudEurekaApplication.class, args);
+    }
+}
