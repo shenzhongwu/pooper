ce64ebb8ee45a6d0ba80b63ad19f084865d3b9f6
shenzhongwu <1263955683@qq.com>
2018-12-11 15:38:26 +0800

    基础提交

--- cloud-eureka/src/main/java/com/poop/application/CloudEurekaApplication.java
+++ cloud-eureka/src/main/java/com/poop/application/CloudEurekaApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class CloudEurekaApplication {
+class CloudEurekaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CloudEurekaApplication.class, args);
